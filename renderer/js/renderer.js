0c69cefad01563ad02e338be7055d36d02674766
Laura Sofia Dueñas Bulla <lauraduenas1105@gmail.com>
2023-06-28 22:16:13 -0500

    check file type of the image

--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -1,1 +1,29 @@
+const form = document.querySelector('#img-form');
+const img = document.querySelector('#img');
+const outputPath = document.querySelector('#output-path');
+const filename = document.querySelector('#filename');
+const heightInput = document.querySelector('#height');
+const widthInput = document.querySelector('#width');
 
+// Load image and show form
+function loadImage(e) {
+  const file = e.target.files[0];
+
+  // Check if file is an image
+  if (!isFileImage(file)) {
+    alertError('Please select an image');
+    return;
+  }
+
+  // Show form, image name and output path
+  form.style.display = 'block';
+  filename.innerText = file.name;
+}
+
+// Make sure file is an image
+function isFileImage(file) {
+  const acceptedImageTypes = ['image/gif', 'image/jpeg', 'image/png'];
+  return file && acceptedImageTypes.includes(file['type']);
+}
+
+img.addEventListener('change', loadImage);
